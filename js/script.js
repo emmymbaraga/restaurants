ee8250c08efb6ef3518b04356fb79100b24512ba
emmymbaraga <emmnish92@gmail.com>
2022-05-20 15:25:41 +0200

    adding menu,feature and contact us section

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// In contact us section
+document.querySelector('#address').innerHTML = 'Kigali-Rwanda';
+document.querySelector('#street').innerHTML = 'KG';
+document.querySelector('#email').innerHTML = 'info@bng.rw';
+document.querySelector('#phone').innerHTML = '+250785491895';
+
+//Footer
+document.getElementById("copyright").innerHTML="BNG.rw &copy; " + new Date().getFullYear();
+
+// Link to social media
+document.querySelector('#facebook').href = 'https://www.facebook.com/emmymbaraga';
+document.querySelector('#twitter').href = 'https://www.twitter.com/EmmanuelMbaraga';
+document.querySelector('#instagram').href = 'https://www.instagram.com/nishimwemanudi/';
+document.querySelector('#linkedin').href = 'https://www.linkedin.com/in/emmanuel-mbaraga-nishimwe-b67750111/';
+document.querySelector('#youtube').href = 'https://www.youtube.com/channel/UCAqvKtKb8OEuax4heSU8GiQ';
+document.querySelector('#github').href = 'https://github.com/emmymbaraga';
+document.querySelector('#whatsapp').href = 'https://wa.me/+250785491895';
